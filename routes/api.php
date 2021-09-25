f4be333f284102e52109053861829d988edc9894
Tilis Tiadi <tiliztiadi@gmail.com>
2021-09-25 12:38:34 +0700

    Membuat Restful API Register

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Api Register
+ */
+Route::post('/register', [RegisterController::class, 'register']);
